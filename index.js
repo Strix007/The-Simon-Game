f472f8fc10429c7961b658b6d6d5dad3dc392cd2
Strix007 <arbabashruff>
2021-07-19 18:53:31 +0530

    Sounds And Restart Work Now

--- index.js
+++ index.js
@@ -16,15 +16,30 @@ function randomColor (){
     $("#" + randomColor).fadeOut(50).fadeIn(50);
     return randomColor;
 }
-randomColor();
 
 // Make An Array Which Stores THe User Colors Clicked By The User 
 
 var userSequence = [];
 
+// Add A Key-Down Event To Document
+
+$(document).on("keydown", function (){
+    if (sequence.length == 0) {
+
+        // Make The Key-Down Event Call randomColor(); Function
+
+        randomColor();
+
+        // Everytime Key-Down Event Is Called Reset "#level-title" To Level Counter Using sequence.length
+
+        $("#level-title").text("Level " + sequence.length);
+
+    }
+})
+
 // Add Event Listeners To All The Buttons With jQuery Using The Mutual Class "btn"
 
-$(".btn").on("click", function (e){
+$(".btn").on("click", function (e) {
     userSequence.push(e.target.id);
     console.log(e.target.id);
 
@@ -32,27 +47,39 @@ $(".btn").on("click", function (e){
 
     var sequenceToString = sequence.toString();
     var userSequenceToString = userSequence.toString();
-    if (sequenceToString === userSequenceToString){
+    if (sequenceToString == userSequenceToString) {
         $("#" + e.target.id).fadeOut(50).fadeIn(50);
-        setTimeout (
-            function (){
-                randomColor();
-            }, 300
-        )
-        console.log("You Won!");
-    } else {
-        userSequence = [];
-        sequence = [];
-        $("#" + e.target.id).addClass("pressed");
+        $("#level-title").text("Level " + sequence.length);
+        playSound(e.target.id);
         setTimeout(
             function () {
-                $("#" + e.target.id).removeClass("pressed")
-            }, 100
-        );
-        setTimeout(
-            function (){
                 randomColor();
             }, 300
         )
+    } else {
+        sequence = [];
+        userSequence = [];
+        $("#" + e.target.id).addClass("pressed");
+        $("body").addClass("game-over");
+        $("#level-title").text("Game Over, Press Any Key To Restart");
+        playSound("wrong");
+        setTimeout(
+            function () {
+                $("#" + e.target.id).removeClass("pressed");
+            }, 100
+        )
+        setTimeout(
+            function () {
+                $("body").removeClass("game-over");
+            }, 200
+        )
     }
-})
+}
+)
+
+// Make A Function With The Name sound With The Parameter clip To Play The Sounds
+
+function playSound (clip){
+    var sound = new Audio("sounds/" + clip + ".mp3");
+    sound.play();
+}
