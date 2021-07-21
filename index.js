4110791f2998f1f3e6286016793b005359698897
Strix007 <arbabashruff>
2021-07-21 17:16:19 +0530

    Game Logic Actually Works Now

--- index.js
+++ index.js
@@ -14,12 +14,35 @@ function randomColor (){
     var randomColor = colors[randomNum];
     sequence.push(randomColor);
     $("#" + randomColor).fadeOut(50).fadeIn(50);
+    playSound(randomColor);
     return randomColor;
 }
 
 // Make An Array Which Stores THe User Colors Clicked By The User 
 
 var userSequence = [];
+
+// Make A Function With The Name sound With The Parameter clip To Play The Sounds
+
+function playSound(clip) {
+    var sound = new Audio("sounds/" + clip + ".mp3");
+    sound.play();
+}
+
+// Make A Function For When The Game Is Over
+
+function gameOver (){
+    sequence = [];
+    userSequence = [];
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(
+        function () {
+            $("body").removeClass("game-over");
+        }, 300
+    )
+    $("#level-title").text("Game Over, Press Any Key To Restart");
+}
 
 // Add A Key-Down Event To Document
 
@@ -37,49 +60,29 @@ $(document).on("keydown", function (){
     }
 })
 
-// Add Event Listeners To All The Buttons With jQuery Using The Mutual Class "btn"
+// Add Event Listeners To All The Buttons With jQuery Using The Mutual Class "btn" And Set The Game Logic
 
-$(".btn").on("click", function (e) {
+$(".btn").on("click", function (e){
+    $("#" + e.target.id).fadeOut(50).fadeIn(50);
     userSequence.push(e.target.id);
-    console.log(e.target.id);
-
-    // Compare sequence Array And userSequence Array Using An If Statement And If They Match Call randomColor();
-
-    var sequenceToString = sequence.toString();
-    var userSequenceToString = userSequence.toString();
-    if (sequenceToString == userSequenceToString) {
-        $("#" + e.target.id).fadeOut(50).fadeIn(50);
-        $("#level-title").text("Level " + sequence.length);
-        playSound(e.target.id);
-        setTimeout(
-            function () {
-                randomColor();
-            }, 300
-        )
+    playSound(e.target.id);
+    if (sequence.length == userSequence.length){
+        var sequenceToLength = sequence.toString();
+        var userSequenceToLength = userSequence.toString();
+        if (sequenceToLength == userSequenceToLength){
+            userSequence = [];
+            setTimeout(
+                function (){
+                    randomColor();
+                }, 300
+            )
+        } else {
+            gameOver();
+        }
     } else {
-        sequence = [];
-        userSequence = [];
-        $("#" + e.target.id).addClass("pressed");
-        $("body").addClass("game-over");
-        $("#level-title").text("Game Over, Press Any Key To Restart");
-        playSound("wrong");
-        setTimeout(
-            function () {
-                $("#" + e.target.id).removeClass("pressed");
-            }, 100
-        )
-        setTimeout(
-            function () {
-                $("body").removeClass("game-over");
-            }, 200
-        )
+        if (sequence[userSequence.length - 1] !== userSequence[userSequence.length - 1]){
+            gameOver();
+        }
     }
 }
 )
-
-// Make A Function With The Name sound With The Parameter clip To Play The Sounds
-
-function playSound (clip){
-    var sound = new Audio("sounds/" + clip + ".mp3");
-    sound.play();
-}
